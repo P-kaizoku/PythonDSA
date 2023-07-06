5d2e0c4d5b8de29414fe81bc138b221e051be4fa
programming-he-ro <83583681+programming-he-ro@users.noreply.github.com>
2023-07-06 16:42:16 +0000

    insert and printLL method added to the linkedlist class

--- main.py
+++ main.py
@@ -2,15 +2,42 @@
 
 class Node:
   #constructor
-  def __init__(self, data, next=None):
+  def __init__(self, data=None, next=None):
     self.data = data
     self.next = next
 
 #A linked list class with a single head node
 class LinkedList:
   def __init__(self):
-    self.head = None
+    self.head = Node()
 
-ll = LinkedList()
-ll.head = Node(1)
-print(ll.head.data)
+
+  def insert(self, value):
+    new_node = Node(value)
+    current = self.head
+    if(current.data):
+      while(current.next):
+        current = current.next
+      current.next = new_node
+    else:
+      self.head = new_node
+
+  def printLL(self):
+    current = self.head
+    while(current.next):
+      print(current.data)
+      print(current.next)
+      current = current.next
+    print(current.data)
+    print(current.next)
+
+
+LL = LinkedList()
+LL.insert(1)
+LL.insert(2)
+LL.insert(3)
+LL.insert(4)
+LL.insert(5)
+LL.insert(5)
+
+LL.printLL()
